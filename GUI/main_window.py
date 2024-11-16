27f27c41b986230adcc862cc1174b92829f8425a
Jekov-Evgen <jena1245481te@gmail.com>
2024-11-17 01:37:03 +0200

    res

--- GUI/main_window.py
+++ GUI/main_window.py
@@ -1,22 +1,30 @@
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QMainWindow, QWidget, QLabel, QPushButton, QLineEdit, QVBoxLayout, QMessageBox
+import pygame
+
+def result():
+    pygame.mixer.music.stop()
+    res = QMessageBox()
+    res.setWindowTitle("Время вышло")
+    res.setText("Вы постарались")
+    res.exec()
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Медитация")
-        self.setFixedSize(350, 250)
+        self.setFixedSize(450, 350)
         self.setWindowIcon(QIcon(r"Image\icon.webp"))
-        
-        self.error = None
         
         control_UI = QVBoxLayout()
         central_widget = QWidget()
         
-        greet = QLabel(text="Введите сколько времени вам нужно для медитации")
+        greet = QLabel(text="Введите сколько времени вам нужно для медитации(время в секундах)")
         self.enter_time = QLineEdit()
+        
         start_meditation = QPushButton(text="Начать медитацию")
+        start_meditation.clicked.connect(self.start_med)
         
         control_UI.addWidget(greet, alignment=Qt.AlignmentFlag.AlignHCenter)
         control_UI.addWidget(self.enter_time)
@@ -30,8 +38,16 @@ class MainWindow(QMainWindow):
         try:
             time = int(self.enter_time.text())
         except:
-            self.error = QMessageBox()
-            self.error.setWindowTitle("Ошибка")
-            self.error.setText("Вводить можно только числа")
+            error = QMessageBox()
+            error.setWindowTitle("Ошибка")
+            error.setText("Вводить можно только числа")
+            error.exec()
+        
+        pygame.mixer.init() 
+        pygame.mixer.music.load(r"Music\Earth Essence - Winds of Meditation.mp3")
+        pygame.mixer.music.play(-1)
+        
+        timing = QTimer()
+        timing.singleShot(time * 1000, result)
             
         
